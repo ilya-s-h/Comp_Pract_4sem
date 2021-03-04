800177f988d9491fb5a2be0e70b35c6046d03ec3
Ilya <60644554+ilya-s-h@users.noreply.github.com>
2021-03-04 19:26:57 +0300

    Update name.js

--- LR 1-2/lib/name.js
+++ LR 1-2/lib/name.js
@@ -1,8 +1,6 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+Object.defineProperty(exports, "__esModule", {value: true });
 exports["default"] = void 0;
-var _default = 'D. Bukin';
+var _default = 'Ilya Shumyakin';
 exports["default"] = _default;
