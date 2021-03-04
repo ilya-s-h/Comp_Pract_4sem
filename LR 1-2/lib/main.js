e68a86662375be0f51734e0a835054a43ab5c593
Ilya <60644554+ilya-s-h@users.noreply.github.com>
2021-03-04 19:26:19 +0300

    Update main.js

--- LR 1-2/lib/main.js
+++ LR 1-2/lib/main.js
@@ -1,9 +1,7 @@
 "use strict";
 
 var _moment = _interopRequireDefault(require("moment"));
-
 var _name = _interopRequireDefault(require("./name"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 console.log(_name["default"]);
