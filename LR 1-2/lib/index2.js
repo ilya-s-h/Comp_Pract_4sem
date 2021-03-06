68a14881788ed726d3bd189557c6a3a51de3301e
Ilya <60644554+ilya-s-h@users.noreply.github.com>
2021-03-06 12:37:39 +0300

    Delete LR 1-2 directory

--- LR 1-2/lib/index2.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _namer = _interopRequireDefault(require("./namer2"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var person = new _namer["default"]();
-console.log(person.getFullName());
--- LR 1-2/src/index2.js
+++
@@ -1,3 +0,0 @@
-import Namer from './namer2';
-const person = new Namer();
-console.log(person.getFullName());
